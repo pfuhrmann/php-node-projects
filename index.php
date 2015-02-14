2346eb941e7fa4c3d6fd30bb40c9df2d1a9b3077
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2015-02-14 16:32:55 +0000

    Cleaning before actual work

--- index.php
+++ index.php
@@ -8,23 +8,11 @@ require 'vendor/autoload.php';
 // Bootstrap Twig
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, ['debug' => true]);
-$twig->addExtension(new COMP1687\CW\SessionTwigExtension());
 $twig->addExtension(new Twig_Extension_Debug());
 
 // Router bootstrap
 $router = new Phroute\RouteCollector();
-// Authorization filter
-$router->filter('auth', function() {
-    if (!isset($_SESSION['user']) || $_SESSION['user']['active'] !== '1') {
-        header("HTTP/1.0 403 Forbidden");
-
-        return "You are not authorized to access this page!";
-    }
-});
-$router->controller('/', new COMP1687\CW\Controllers\AuthenticationController($twig));
-$router->controller('/', new COMP1687\CW\Controllers\ServicesController($twig), ['before' => 'auth']);
-$router->controller('/', new COMP1687\CW\Controllers\SearchController($twig));
-$router->controller('/', new COMP1687\CW\Controllers\AjaxController());
+$router->controller('/', new COMP1687\CW\Controllers\ServicesController($twig));
 $dispatcher = new Phroute\Dispatcher($router);
 
 // This is hack for stuweb web server
