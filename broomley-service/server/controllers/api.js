5f2c78de99b068ffc3027e8be28226a703d9d3ae
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2015-03-20 23:47:34 +0000

    Progress for level 8

---
+++ broomley-service/server/controllers/api.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+* Get sitters
+*
+* HTTP GET /sitters
+*/
+exports.sitters = function(req, res) {
+    res.send('hello world');
+};
+
+/*
+ * Get sitters
+ *
+ * HTTP GET /sitter-details
+ */
+exports.sitterDetails = function(req, res) {
+    res.send('hello world');
+};
